9e3c6aa9a6fdc5cd949dc82a7ec1ed3e6fe00d23
TUSHAR MAKAVANA <makavanatushar2345@gmail.com>
2025-09-23 10:16:39 +0530

    chnage resume link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -97,7 +97,7 @@ const Hero = () => {
             Hire Me
           </motion.a>
           <motion.a
-            href="https://drive.google.com/file/d/1STmpIk0YdzGf6z9bXNfEsT2FzIVkpr0B/view"
+            href="https://drive.google.com/file/d/1T7LLUFGaYI6DpBaH5Gb_4C6W9dwJNC5K/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
             className={`px-6 py-3 rounded-xl shadow-lg font-semibold border-2 transition-colors duration-200 ${cvBtn}`}
